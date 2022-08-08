ffa9312f44a950adf4bea687acb621728acce91a
Chris Martin <chris.martin@oforce.com>
2022-08-08 10:35:16 -0700

    fix comment

--- lib/thetamind/tasks/aggregates/node.ex
+++ lib/thetamind/tasks/aggregates/node.ex
@@ -7,7 +7,7 @@ defmodule Thetamind.Tasks.Aggregates.Node do
   def execute(%{id: nil}, %CreateNode{} = command), do: NodeCreated.new(command)
   def execute(_state, %CreateNode{}), do: nil
 
-  # Nothing get's past here with an ID
+  # Nothing gets past here without an ID
   def execute(%{id: nil}, _command), do: {:error, "node not found"}
 
   def execute(_state, %DeleteNode{} = command), do: NodeDeleted.new(command)
